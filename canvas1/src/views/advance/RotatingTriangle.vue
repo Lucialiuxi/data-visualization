ce3c079de3b6519c9ca8626631c9e906826bf5e1
Lucialiuxi <1435554190@qq.com>
2023-06-14 11:22:16 +0800

    rotating translate animation

--- canvas1/src/views/advance/RotatingTriangle.vue
+++ canvas1/src/views/advance/RotatingTriangle.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="canvas-rotating-triangle">
+    <button type="button" class="up" @click="changeDirection('up')">up</button>
+    <button type="button" class="down" @click="changeDirection('down')">down</button>
     <canvas id="rotating-triangle" height="600" width="600"></canvas>
   </div>
 </template>
@@ -10,8 +12,23 @@ import { getWebGLContext, initShaders } from '@lib/cuon-utils';
 import Matrix4 from '@lib/cuon-matrix';
 
 export default {
+  data(){
+    return {
+      startTime: Date.now(),
+      gl_last: Date.now(), // 记录上一次调用绘制三角形的时间
+      ANGLE_STEP: 45.0, // 旋转速度 每秒旋转45度
+      currentAngle: 0.0, // 三角形当前旋转角度
+      count: 0, // 记录动画时长
+      Ty: 0, // y轴平移方向
+      direction: '',
+    }
+  },
   mounted() {
     this.paintHandle();
+  },
+  updated() {
+    console.log('456', this.Ty)
+    
   },
   methods: {
     paintHandle() {
@@ -52,20 +69,21 @@ export default {
       }
 
       let u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
-      gl.uniform4f(u_FragColor, 0.5, 0.0, 0.0, 1.0);
+      gl.uniform4f(u_FragColor, 0.3, 0.5, 0.0, 1.0);
 
-      gl.clearColor(0.0, 0.3, 0.1, 1.0);
-      gl.clear(gl.COLOR_BUFFER_BIT);
+      gl.clearColor(0.0, 0.3, 0.3, 1.0);
 
-      gl.drawArrays(gl.TRIANGLES, 0, n);
+      let  u_xformMatrix = gl.getUniformLocation(gl.program, 'u_xformMatrix');
+
+      this.tick(gl, u_xformMatrix, n, Matrix4, this.Ty);
     },
     initVertexBuffer(gl) {
       let n = 3;
       let vertices = new Float32Array(6);
       vertices.set([
           0.0, 0.5,
-          0.5, 0.0,
-          -0.5, 0.0,
+          -0.5, -0.5,
+          0.5, -0.5,
       ]);
 
       let vertexBuffer = gl.createBuffer();
@@ -76,12 +94,54 @@ export default {
       // 告诉显卡重当前绑定的缓冲区中读取顶点数据
       gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
       gl.enableVertexAttribArray(a_Position);
-      
-      Matrix4.setTranslate(0.2, 0.1, 0);
-      let  u_xformMatrix = gl.getUniformLocation(gl.program, 'u_xformMatrix');
-      gl.uniformMatrix4fv(u_xformMatrix, false, Matrix4.elements);
       return n;
-    }
+    },
+    drawHandle(gl, u_xformMatrix, n, angle, matrix_model,Ty) {
+      // 设置旋转矩阵
+      Matrix4.setRotate(angle, 0, 0, 1);
+      Matrix4.translate(0.3, Ty, 0);
+      // 将旋转矩阵传输给顶点着色器
+      gl.uniformMatrix4fv(u_xformMatrix, false, matrix_model.elements);
+      // 清除canvas
+      gl.clear(gl.COLOR_BUFFER_BIT);
+      // 绘制三角形
+      gl.drawArrays(gl.TRIANGLES, 0, n);
+    },
+    animateHandle(angle){
+      // 计算距离上次调用经过多长的时间
+      let now = Date.now();
+      let elapsed = now - this.gl_last; // 毫秒
+      this.gl_last = now;
+      // 根据距离上一次调用的时间， 更新当前旋转角度
+      let newAngle = angle + (this.ANGLE_STEP * elapsed) / 1000;
+
+      return newAngle %= 360; // 取余数
+    },
+    tick(gl, u_xformMatrix, n, matrix_model, Ty) {
+      if (this.gl_last - this.startTime < 60000) {
+        this.currentAngle = this.animateHandle(this.currentAngle); // 更新旋转角度
+        this.drawHandle(gl, u_xformMatrix, n, this.currentAngle, matrix_model, Ty);
+        
+        // 告诉浏览器执行一个动画，并且要求浏览器在下次重绘之前调用制定的回调函数更新动画
+        requestAnimationFrame(() => {
+          this.tick(gl, u_xformMatrix, n, matrix_model, Ty);
+        }); 
+      } else {
+        // 停止动画
+        window.cancelAnimationFrame(this.tick);
+      }
+    },
+    changeDirection(direction) {
+      if (this.direction !== direction) {
+        this.direction = direction;
+      }
+      if (direction === 'up') {
+        this.Ty += 0.1;
+      } else if (direction === 'down') {
+        this.Ty -= 0.1;
+      }
+      console.log('34234--', this.Ty)
+    },
   }
 }
 </script>
