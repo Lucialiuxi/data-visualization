2f2b9ba0e290f10e28a5bddfac424546d36d123b
Lucialiuxi <1435554190@qq.com>
2023-06-28 16:02:14 +0800

    base

--- canvas1/src/views/threeDimensionalSpace/LookAtTriangles.vue
+++ canvas1/src/views/threeDimensionalSpace/LookAtTriangles.vue
@@ -6,7 +6,7 @@
 
 <script>
 import { getWebGLContext, initShaders } from '@lib/cuon-utils.js';
-
+import Matrix4 from '@lib/cuon-matrix.js';
 
 export default {
     mounted() {
@@ -15,20 +15,29 @@ export default {
     methods: {
         paintHandle() {
             let VSHADER_SOURCE = `
+                precision mediump float;
                 attribute vec4 a_Position;
 
+                attribute vec4 a_Color;
+                varying vec4 v_Color;
+
+                uniform mat4 u_ViewMatrix;
+
                 void main() {
-                    gl_Position = a_Position;
+                    gl_Position = u_ViewMatrix * a_Position;
+                    v_Color = a_Color;
                 }
             `;
             // gl_FragCoord 是内置变量，用来表示片元的坐标
             let FSHADER_SOURCE = `
                 precision mediump float;
+
                 uniform float u_Width;
                 uniform float u_Height;
+                varying vec4 v_Color;
 
                 void main() {
-                    gl_FragColor = vec4( gl_FragCoord.x/u_Width, 0.0, gl_FragCoord.y/u_Height, 1.0);
+                    gl_FragColor = v_Color;
                 }
             `;
 
@@ -51,10 +60,17 @@ export default {
                 return;
             }
 
-            let u_Width = gl.getUniformLocation(gl.program, 'u_Width');
-            let u_Height = gl.getUniformLocation(gl.program, 'u_Height');
-            gl.uniform1f(u_Width, gl.drawingBufferWidth);
-            gl.uniform1f(u_Height, gl.drawBufferHeight);
+            let a_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix');
+
+            // 设置 视点、观察目标点 和 上方向
+            Matrix4.setLookAt(
+                0.20, 0.25, 0.25, // 视点
+                0, 0, 0, // 观察目标点
+                0, 1, 0, // 上方向
+            );
+            // 将视图矩阵传给u_ViewMatrix
+            gl.uniformMatrix4fv(a_ViewMatrix, false, Matrix4.elements), 
+
 
             gl.clearColor(0.1, 0.2, 0.3, 1.0);
             gl.clear(gl.COLOR_BUFFER_BIT);
@@ -63,15 +79,37 @@ export default {
         },
         // 初始化缓冲区对象
         initVertexBuffer(gl) {
+            // 绿色三角形 在最后面
+            const triangle1 = [
+                0.0, 0.5, -0.4, 0.4, 1.0, 0.4, // x, y, z, r, g, b
+                -0.5, -0.5, -0.4, 0.4, 1.0, 0.4,
+                0.5, -0.5, -0.4, 1.0, 0.4, 0.4,
+            ];
+
+            // 黄色三角形 在中间
+            const triangle2 = [
+                0.5, 0.4, -0.2, 1.0, 0.4, 0.4,
+                -0.5, 0.4, -0.2, 1.0, 1.0, 0.4,
+                0.0, -0.6, -0.2, 1.0, 0.4, 0.4,
+            ];
+
+            // 蓝色三角形 在最前面
+            const triangle3 = [
+                0.0, 0.5, 0.0, 0.4, 0.4, 1.0,
+                -0.5, -0.5, 0.0, 0.4, 0.4, 1.0,
+                0.5, -0.5, 0.0, 1.0, 0.4, 0.4,
+            ];
+            let len = triangle1.length + triangle2.length + triangle3.length;
             // 顶点位置
             let vertices = new Float32Array([
-                0.0, 0.5, 
-                -0.5, -0.5,
-                0.5, - 0.5
-            ], 0, 6);
+                ...triangle1,
+                ...triangle2,
+                ...triangle3,
+            ], 0, len);
 
             // 顶点个数 
-            let n = 3;
+            let n = 9;
+            const FSIZE = vertices.BYTES_PER_ELEMENT;
 
             let vertexBuffer = gl.createBuffer();
             if(!vertexBuffer) {
@@ -81,7 +119,10 @@ export default {
             gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
             let a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-            gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+            gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, FSIZE * 6, 0);
+
+            let a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+            gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 3);
 
             gl.enableVertexAttribArray(a_Position);
             return n;
