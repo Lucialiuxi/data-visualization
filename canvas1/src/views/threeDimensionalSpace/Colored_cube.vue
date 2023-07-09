51dfba2bdaa638d79154f7d9ebde91b987b6a6ff
Lucialiuxi <1435554190@qq.com>
2023-07-09 14:24:58 +0800

    save wrong version

--- canvas1/src/views/threeDimensionalSpace/Colored_cube.vue
+++ canvas1/src/views/threeDimensionalSpace/Colored_cube.vue
@@ -63,8 +63,8 @@ export default {
                 varying vec4 v_Color;
 
                 void main() {
-                    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-                    //gl_FragColor = v_Color;
+                    //gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+                    gl_FragColor = v_Color;
                 }
             `;
 
@@ -89,7 +89,7 @@ export default {
             let viewMatrix = new Matrix4();
             let projMatrix = new Matrix4();
             viewMatrix.setLookAt(
-                3, 3, 3, // 视点
+                -3, -3, 13, // 视点
                 0, 0, 0, // 目标点
                 0, 1, 0, // 上方向
             );
@@ -110,40 +110,60 @@ export default {
             gl.drawElements(gl.TRIANGLE_FAN, n, gl.UNSIGNED_BYTE, 0);
         },
         initVertexBuffers(gl) {
-            /**
-             *  白色 1.0, 1.0, 1.0
-             *  品红色 0.67, 0, 0.73
-             *  红色   1.0, 0.0, 0.0
-             *  黄色   1.0, 1.0, 0.0
-             * 
-             * 蓝色 0.0, 0.0, 1.0
-             * 黑色 0.0, 0.0, 0.0
-             * 青色 0.4, 0.6, 0.6
-             * 绿色 0.0, 0.62, 0.42
-             */
-
+            let colorObj = {
+                magenta: [ 0.67, 0, 0.73 ], // 品红色
+                red: [ 1.0, 0.0, 0.0 ],
+                yellow: [ 1.0, 1.0, 0.0 ],
+                blue: [ 0.0, 0.0, 1.0 ],
+                cyan: [ 0.4, 0.6, 0.6 ], // 青色
+                green: [ 0.0, 0.62, 0.42 ],
+            };
+            let axisObj = {
+                v0: [ 1.0, 1.0, 1.0 ],
+                v1: [ -1.0, 1.0, 1.0 ],
+                v2: [ -1.0, -1.0, 1.0 ],
+                v3: [ 1.0, -1.0, 1.0 ],
+                v4: [ 1.0, -1.0, -1.0 ],
+                v5: [ 1.0, 1.0, -1.0 ],
+                v6: [ -1.0, 1.0, -1.0 ],
+                v7: [ -1.0, -1.0, -1.0 ],
+            }
              // 顶点坐标
             let vertexAxis = [
-                0.3, 0.3, 0.3, // v0
-                -0.3, 0.3, 0.3, // v1
-                -0.3, -0.3, 0.3, // v2
-                0.3, -0.3, 0.3, // v3
-                0.3, -0.3, -0.3, // v4
-                0.3, 0.3, -0.3, // v5
-                -0.3, 0.3, -0.3, // v6
-                -0.3, -0.3, -0.3, // v7
+                // 正面
+                ...axisObj.v0, ...axisObj.v1, ...axisObj.v2, ...axisObj.v3,
+                // 右面
+                ...axisObj.v0, ...axisObj.v3, ...axisObj.v4, ...axisObj.v5,
+                // 正上面
+                ...axisObj.v0, ...axisObj.v5, ...axisObj.v6, ...axisObj.v1,
+                // 正下面
+                ...axisObj.v7, ...axisObj.v2, ...axisObj.v3, ...axisObj.v4,
+                // 正后面
+                ...axisObj.v7, ...axisObj.v4, ...axisObj.v5, ...axisObj.v6,
+                // 左面
+                ...axisObj.v7, ...axisObj.v6, ...axisObj.v1, ...axisObj.v2,
             ];
 
             // 顶点颜色
             let vertexColor = [
-                0.67, 0.0, 0.73, // 品红色
-                1.0, 0.0, 0.0, // 红色
-                1.0, 1.0, 0.0, // 黄色
-                0.0, 0.0, 1.0, // 蓝色
-                0.4, 0.6, 0.6, // 青色
-                0.0, 0.62, 0.42, // 绿色
+                // 正面
+                ...colorObj.green, ...colorObj.green, ...colorObj.green, ...colorObj.green,
+                // 右面
+                ...colorObj.magenta, ...colorObj.magenta, ...colorObj.magenta, ...colorObj.magenta,
+
+                // 正上面
+                ...colorObj.red, ...colorObj.red, ...colorObj.red, ...colorObj.red,
+
+                // 正下面
+                ...colorObj.yellow, ...colorObj.yellow, ...colorObj.yellow, ...colorObj.yellow,
+
+                // 正后面
+                ...colorObj.blue, ...colorObj.blue, ...colorObj.blue, ...colorObj.blue, ...colorObj.blue,
+
+                // 左面
+                ...colorObj.cyan, ...colorObj.cyan, ...colorObj.cyan, ...colorObj.cyan,
             ];
-            
+
              // 顶点坐标
             let vertices = new Float32Array(vertexAxis, 0, vertexAxis.length);
             this.initArrayBuffer(gl, vertices, 'a_Position');
@@ -151,32 +171,21 @@ export default {
             let colors = new Float32Array(vertexColor, 0, vertexColor.length);
             this.initArrayBuffer(gl, colors, 'a_Color');
 
-            // 顶点索引
-            let vertexIndices = [
+            // 索引
+            let indices = [
                 0, 1, 2, 3,// 正面
-                0, 3, 4, 5, // 右面
-                0, 5, 6, 1, // 上面
-                7, 2, 3, 4,// 下面
-                7, 4, 5, 6, // 背面
-                7, 2, 1, 6,// 左面
-            ];
-
-            // 颜色索引
-            let colorIndices = [
-                0, 0, 0, 0,
-                1, 1, 1, 1,
-                2, 2, 2, 2,
-                3, 3, 3, 3,
-                4, 4, 4, 4,
-                5, 5, 5, 5,
+                4, 5, 6, 7, // 右面
+                8, 9, 10, 11, // 上面
+                12, 12, 14, 15,// 下面
+                16, 17, 18, 19, // 背面
+                20, 21, 22, 23,// 左面
             ];
             // 立方体每个面的三角扇顶点索引【绘制顺序】
-            this.initElementArrayBuffer(gl, vertexIndices);
+            this.initElementArrayBuffer(gl, indices);
 
-            this.initElementArrayBuffer(gl, colorIndices);
-
-            return vertexIndices.length;
+            return indices.length;
         },
+        // 初始化顶点缓冲对象
         initArrayBuffer(gl, typeArray, attribAttr) {
             let buffer  = gl.createBuffer();
             if (!buffer) return -1;
@@ -187,6 +196,7 @@ export default {
             gl.enableVertexAttribArray(attrib);
             return true;
         },
+        // 初始化顶点缓冲对象
         initElementArrayBuffer(gl, indices) {
             // 立方体每个面的三角扇顶点索引【绘制顺序】
             let index = new Uint8Array(indices, 0, indices.length);
