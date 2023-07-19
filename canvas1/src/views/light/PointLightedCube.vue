f395b139491ad9f7002d8b4bde8a49e84c07caf2
Lucialiuxi <1435554190@qq.com>
2023-07-19 19:30:21 +0800

    save wrong version

--- canvas1/src/views/light/PointLightedCube.vue
+++ canvas1/src/views/light/PointLightedCube.vue
@@ -42,7 +42,7 @@ export default {
 
                     vec3 diffuseColor = u_LightColor * a_Color.rgb * dotLN;
 
-                    v_Color = vec4(diffuseColor, a_Color.b);
+                    v_Color = vec4(diffuseColor, a_Color.a);
                 }
 
             `;
@@ -85,7 +85,7 @@ export default {
              *  type 指定元素数组缓冲区中的值的类型
              *  offset 指定元素数组缓冲区中的偏移量
              */
-            gl.drawElements(gl.TRIANGLE_STRIP, n, gl.UNSIGNED_BYTE, 0);
+            gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
         },
         lightHandle(gl) {
             let u_LightColor = gl.getUniformLocation(gl.program, 'u_LightColor');
@@ -108,7 +108,7 @@ export default {
                 100,
             );
             mvpMatrix.lookAt(
-                3, 3, 7,
+                -3, -3, 7,
                 0, 0, 0,
                 0, 1, 0,
             );
@@ -127,30 +127,37 @@ export default {
                 v6 = [ -1.0, 1.0, -1.0 ],
                 v7 = [ -1.0, -1.0, -1.0 ];
 
-            let vertexAxis = [ 
-                // 正面 
-                ...v1, ...v2, ...v0, ...v3,
-                // 背面
-                ...v6, ...v7, ...v5, ...v4,
+             // 顶点坐标
+             let vertexAxis = [
+                // 正面
+                ...v0, ...v1, ...v2, ...v3,
+                // 右面
+                ...v0, ...v3, ...v4,...v5,
+                // 上面
+                ...v0, ...v5, ...v6, ...v1,
                 // 左面
-                ...v2, ...v7, ...v1, ...v6,
-                // 右面 
-                ...v3, ...v4, ...v0, ...v5,
-                // 顶面 
-                ...v1, ...v6, ...v0, ...v5,
-                // 底面 
-                ...v2, ...v7, ...v3, ...v4,
+                ...v1, ...v6, ...v7, ...v2, 
+                // 下面
+                ...v7, ...v4, ...v3, ...v2,
+                // 背面
+                ...v4, ...v7, ...v6, ...v5,
             ];
 
 
-            let red = [ 1.0, 1.0, 0.0 ];
+            let magenta = [ 0.67, 0, 0.73 ],
+                red = [ 1.0, 0.0, 0.0 ],
+                yellow = [ 1.0, 1.0, 0.0 ],
+                blue = [ 0.0, 0.0, 1.0 ],
+                // 青色
+                cyan = [ 0.4, 0.6, 0.6 ],
+                green = [ 0.0, 0.62, 0.42 ]; 
             let colors = [ 
+                ...magenta, ...magenta, ...magenta, ...magenta, 
                 ...red, ...red, ...red, ...red, 
-                ...red, ...red, ...red, ...red, 
-                ...red, ...red, ...red, ...red, 
-                ...red, ...red, ...red, ...red, 
-                ...red, ...red, ...red, ...red, 
-                ...red, ...red, ...red, ...red, 
+                ...yellow, ...yellow, ...yellow, ...yellow, 
+                ...blue, ...blue, ...blue, ...blue, 
+                ...cyan, ...cyan, ...cyan, ...cyan, 
+                ...green, ...green, ...green, ...green, 
             ];
             // 每个面的法向量
             let top = [  0.0, 1.0, 0.0 ], 
@@ -162,26 +169,23 @@ export default {
             // 法向量
             let normals = [
                 ...front, ...front, ...front, ...front, 
-                ...back, ...back, ...back, ...back, 
-                ...left, ...left, ...left, ...left, 
                 ...right, ...right, ...right, ...right, 
                 ...top, ...top, ...top, ...top, 
+                ...left, ...left, ...left, ...left, 
                 ...bottom, ...bottom, ...bottom, ...bottom, 
+                ...back, ...back, ...back, ...back, 
             ];
-            let indices = [
-                // 正面 
-                0, 1, 2, 3,
-                // 背面
-                4, 5, 6, 7,
-                // 左面 
-                8, 9, 10, 11,
-                // 右面 
-                12, 13, 14, 15,
-                // 顶面 
-                16, 17, 18, 19,
-                // 底面 
-                20, 21, 22, 23
+             // 索引
+             let indices = [
+                0, 1, 2,   0, 2, 3,    // front
+                4, 5, 6,   4, 6, 7,    // right
+                8, 9,10,   8,10,11,    // top
+                12,13,14,  12,14,15,    // left
+                16,17,18,  16,18,19,    // bottom
+                20,21,22,  20,22,23     // back
             ];
+            console.log(vertexAxis.length, colors.length, normals.length)
+
 
             let vertexArray = new Float32Array(vertexAxis, 0, vertexAxis.length);
             this.initArrayBuffer(gl, 'a_Position', vertexArray);
